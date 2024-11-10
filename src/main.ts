80fa4ecd376d3a6190d1310d7181a09c226d4a3c
Izy <126877709+izy-code@users.noreply.github.com>
2024-11-11 00:48:18 +0300

    feat: add user endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DEFAULT_PORT } from './common/constants';
+import { ValidationPipe } from '@nestjs/common';
+
+const port = process.env.PORT || DEFAULT_PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(4000);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(port, () =>
+    console.log(`Application is listening on port: ${port}`),
+  );
 }
+
 bootstrap();
