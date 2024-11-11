5d266525614aa644b429dd0ac297479ff1618c39
Izy <126877709+izy-code@users.noreply.github.com>
2024-11-11 19:27:03 +0300

    feat: add album endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { DatabaseModule } from './database/database.module';
+import { ArtistModule } from './artist/artist.module';
+import { AlbumModule } from './album/album.module';
+import { TrackModule } from './track/track.module';
+import { FavoritesModule } from './favorites/favorites.module';
 
 @Module({
-  imports: [UserModule, DatabaseModule],
+  imports: [
+    UserModule,
+    DatabaseModule,
+    ArtistModule,
+    AlbumModule,
+    TrackModule,
+    FavoritesModule,
+  ],
 })
 export class AppModule {}
