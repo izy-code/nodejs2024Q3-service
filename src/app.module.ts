74a3cee1e4b884c40c79ba0ccb898decdde998f5
Izy <126877709+izy-code@users.noreply.github.com>
2024-11-18 19:09:07 +0300

    feat: add prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { DatabaseModule } from './database/database.module';
+import { PrismaModule } from './prisma/prisma.module';
 import { ArtistModule } from './artist/artist.module';
 import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
@@ -9,7 +9,7 @@ import { FavoritesModule } from './favorites/favorites.module';
 @Module({
   imports: [
     UserModule,
-    DatabaseModule,
+    PrismaModule,
     ArtistModule,
     AlbumModule,
     TrackModule,
