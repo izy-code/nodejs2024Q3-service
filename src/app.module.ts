0bb374bc564f5ca8beb6ebe49b89bb1697789169
Izy <126877709+izy-code@users.noreply.github.com>
2024-11-24 17:29:10 +0300

    feat: add auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { AlbumModule } from './album/album.module';
 import { TrackModule } from './track/track.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { LoggerModule } from './logger/logger.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { LoggerModule } from './logger/logger.module';
     TrackModule,
     FavoritesModule,
     LoggerModule,
+    AuthModule,
   ],
 })
 export class AppModule {}
